a54c76814f4d7b5bee3b23702f7e19742f78318f
Bram Janssens <s.a.janssens@gmail.com>
2025-01-16 12:19:30 +0100

    First example TGIF.

---
+++ src/test/java/nl/bramjanssens/StepDefinitions.java
@@ -0,0 +1,39 @@
+package nl.bramjanssens;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static nl.bramjanssens.IsItFriday.isItFriday;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StepDefinitions {
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        today = "Sunday";
+    }
+
+    @Given("today is Friday")
+    public void today_is_Friday() {
+        today = "Friday";
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+}
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
